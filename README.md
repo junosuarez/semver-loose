5b6ee43992c5f569ce9f7d9ac812e63a722a3489
jden <jason@denizac.org>
2013-04-16 17:09:29 -0700

    initial

---
+++ README.md
@@ -0,0 +1,108 @@
+# semver-loose
+loose precision matching on semvers
+
+## usage
+
+example:
+
+    var semverLoose = require('semver-loose')
+
+    semverLoose.match('1.x', '1.0.3')
+    // => true
+
+    var isMatch = semverLoose.match('1.4+')
+    isMatch('1.3.3')
+    // => false
+    isMatch('1.4')
+    // => true
+    isMatch('1.8.2')
+    // => true
+    isMatch('2.0')
+    // => false
+
+Matching patterns are simpler (albeit less expressive) than npm's [semver](https://npmjs.org/package/semver) matcher.
+
+Patterns can be specified with any precision:
+
+    1     // major only
+    1.x   // major with explict wildcard (equivalent to above)
+    1.2   // major and minor
+    1.2.x // major and minor with wildcard (equivalent to above)
+    1.4.2 // explicit (exact match)
+
+Minimum version ranges can be specified by adding a `+` to the segment, as in `1.2+`. This will match earlier segments exactly, and the current segment with any number greater than or equal to the number specified.
+
+    0.2+  // for this pattern
+    0.2.1 // would match
+    0.4.9 // would match
+    1.2.0 // would not match
+
+Use your head. In practice, you'll only want to use gte matching on minor versions. If you're worried about patch versions, you probably want to use exact matching.
+
+If a tag is specified, it must be stringwise equal to match (although version numbers are still matched the same. This way, for example, release channels can be specified)
+
+    1.x.x-foo // for this pattern
+    1.0.4-foo // would match
+
+    1.2+-foo   // with a gte segment
+    1.5.0-foo  // would match
+    1.1.0-foo  // would not match
+    1.3.0      // would not match
+    1.2.0-foo2 // would not match
+
+See `~/test/test.js` for more examples.
+
+## api
+using (jsig notation)[https://github.com/jden/jsig]
+
+### `match: (range: String, version: String) => Boolean`
+
+may also be used curried: `match: (range: String) => (version: String) => Boolean`. Use curried if matching one version pattern against many possible versions.
+
+Returns true if `version` satisfies `range`, otherwise false.
+
+
+### `compare: (range: Semver, version: Semver) => Boolean`
+
+Mostly you'll want to use `match`. But, if you happen to already have parsed `Semver`s, this function is strictly the comparison algorithm. Returns true if `version` satisfies `range`, otherwise false.
+
+### `parse: (str) => Semver`
+
+`Semver` is an object with the following properties:
+
+    Semver: {
+      major: Number?,
+      minor: Number?,
+      patch: Number?,
+      tag: String?,
+      gte: Boolean
+    }
+
+Corresponding to the respective segments as defined in the (semver spec)[http://semver.org/]
+
+`gte` is true if the semver is a range and should match other semvers which are greater than or equal to itself.
+
+### `sort: (a: Semver, b: Semver) => Number`
+
+Returns a negative if `a` is before `b`, 0 if they are sorted equivalent, and positive if `a` is after `b`. May be used as a sort function with (`Array.sort`)[https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/sort]
+
+Tags are ignored for the purpose of sorting. That is, `1.0.0-pre` is sort-wise equal to `1.0.0`.
+
+## installation
+
+    $ npm install semver-loose
+
+## running the tests
+
+From package root:
+
+    $ npm install
+    $ npm test
+
+## contributors
+
+jden <jason@denizac.org>
+
+## license
+
+(c) 2013 Agile Diagnosis, Inc. <hello@agilediagnosis.com>. See LICENSE.md
---
+++ test/test.js
@@ -0,0 +1,130 @@
+var chai = require('chai')
+chai.should()
+
+var sl = require('../index')
+
+describe('semver-loose', function () {
+
+  describe('match', function () {
+    it('equivalent semvers', function () {
+
+      sl.match('1', '1.2.3').should.equal(true)
+      sl.match('1', '1.0.0').should.equal(true)
+      sl.match('1', '2.0.0').should.equal(false)
+
+
+      sl.match('1.2', '1.2.0').should.equal(true)
+      sl.match('1.2', '1.2.1').should.equal(true)
+      sl.match('1.2', '1.4.1').should.equal(false)
+
+      sl.match('1.2.3', '1.2.3').should.equal(true)
+      sl.match('1.0.1', '2.0.0').should.equal(false)
+      sl.match('1.0.1', '1.0.0').should.equal(false)
+      sl.match('1.0.1', '1.1.0').should.equal(false)
+
+      sl.match('1.2.3-a', '1.2.3-a').should.equal(true)
+      sl.match('2.0.0-a', '2.0.0-b').should.equal(false)
+
+    })
+    it('wildcards', function () {
+      sl.match('1.x', '1.0.0').should.equal(true)
+    })
+    it('loose', function () {
+      sl.match('1', '1.0.0').should.equal(true)
+    })
+    it('gte', function () {
+      sl.match('1.2+', '1.2.0').should.equal(true)
+      sl.match('1.2+', '1.2.5').should.equal(true)
+      sl.match('1.2+', '1.3.0').should.equal(true)
+      sl.match('1.2+', '1.3.4').should.equal(true)
+      sl.match('1.2+', '1.1.999').should.equal(false)
+      sl.match('1.2+', '2.4.0').should.equal(false)
+      sl.match('3+','17').should.equal(true)
+      sl.match('1.4+', '1.8.2').should.equal(true)
+    })
+    it('doesnt wildcard tags', function () {
+      sl.match('1.0.0', '1.0.0-pre').should.equal(false)
+    })
+    it('can be curried', function () {
+      var matcher = sl.match('1.x')
+      matcher('1.0.5').should.equal(true)
+      matcher('2.4.0').should.equal(false)
+    })
+  })
+
+  describe('sort', function () {
+    it('parsed semvers', function () {
+      sl.sort(sl.parse('1.2'), sl.parse('1.2.1')).should.be.below(0)
+      sl.sort(sl.parse('1.0.0'), sl.parse('1.2.1')).should.be.below(0)
+      sl.sort(sl.parse('1.2.3'), sl.parse('2.2.1')).should.be.below(0)
+      sl.sort(sl.parse('1.3.5'), sl.parse('1.3.7')).should.be.below(0)
+
+      sl.sort(sl.parse('1.2'), sl.parse('1.2')).should.equal(0)
+      sl.sort(sl.parse('1'), sl.parse('1.0.0')).should.equal(0)
+      sl.sort(sl.parse('1.2.4'), sl.parse('1.2.4')).should.equal(0)
+
+      sl.sort(sl.parse('0.4.5'), sl.parse('0.3.2')).should.be.above(0)
+      sl.sort(sl.parse('1.8'), sl.parse('1.6.3')).should.be.above(0)
+      sl.sort(sl.parse('1'), sl.parse('0.4.2')).should.be.above(0)
+    })
+  })
+
+  describe('parse', function () {
+    it('parses into an object', function () {
+      sl.parse('5.4.3-21launch').should.deep.equal({
+        major: 5,
+        minor: 4,
+        patch: 3,
+        tag: '21launch',
+        gte: -1
+      })
+    })
+
+    it('parses gte for the highest segment ending with a plus sign', function () {
+      sl.parse('1').gte.should.equal(-1)
+      sl.parse('1+').gte.should.equal(0)
+      sl.parse('1.4+').gte.should.equal(1)
+      sl.parse('1.4.2+').gte.should.equal(2)
+    })
+
+    it('doesnt choke on numbers', function () {
+      sl.parse(3)
+    })
+
+    it('doesnt choke on non-toStringables', function () {
+      sl.parse(null)
+    })
+
+    it('parses numbers IFF a segment is a number', function () {
+      sl.parse('1.0.x').should.deep.equal({
+        major: 1,
+        minor: 0,
+        patch: undefined,
+        tag: undefined,
+        gte: -1
+      })
+      sl.parse('1.x').should.deep.equal({
+        major: 1,
+        minor: undefined,
+        patch: undefined,
+        tag: undefined,
+        gte: -1
+      })
+      sl.parse('x').should.deep.equal({
+        major: undefined,
+        minor: undefined,
+        patch: undefined,
+        tag: undefined,
+        gte: -1
+      })
+    })
+  })
+
+  describe('format', function () {
+
+  })
+
+  it('parses semvers', function () {
+
+  })
+})
